4e7aa3dde13440899fb43397b946dbb8a719978e
Jakksan <degruiter99@gmail.com>
2018-10-15 13:56:56 -0500

    Implemented working DrawHouse Function

--- neighborhood.py
+++ neighborhood.py
@@ -2,6 +2,7 @@ from turtle import *
 import math
 import time
 
+setup(1000,1000)
 goto(0,0)
 penup()
 
@@ -13,7 +14,8 @@ def drawTriangle(x, y, tri_base, tri_height):
 
     top_angle = 180 - (2 * base_angle)
 
-
+    penup()
+    goto(x, y)
     pendown()
     setheading(0)
     forward(tri_base)
@@ -26,6 +28,8 @@ def drawTriangle(x, y, tri_base, tri_height):
 #drawTriangle(0, 0, 250, 40)
 
 def drawRectangle(x, y, rec_width, rect_height):
+    penup()
+    goto(x, y)
 
     pendown()
     setheading(0)
@@ -36,10 +40,12 @@ def drawRectangle(x, y, rec_width, rect_height):
         forward(rect_height)
         left(90)
 
+    penup()
+
 #drawRectangle(0, 0, 50, 55)
 
 def drawCircle(x, y, radius):
-
+    penup()
     setheading(0)
     setpos(x, (y-radius))
     pendown()
@@ -54,13 +60,47 @@ def drawTree(x, y, tree_height):
     drawCircle(x, (y + (tree_height * 0.75)), (tree_height * 0.25))
 
 def drawHouse(x, y, house_width, house_height):
-    door_width = house_width/8
-    door_height = house_height/4
-    drawRectangle(x, y, house_width, house_height*0.8)
-    goto((x+(house_width*0.75)), y)
-    drawRectangle(x, y, door_width, door_height)
-    goto(x+(house_width/2), (y+house_height))
-    drawTriangle(xcor(), ycor(), house_width+10, house_height*0.2)
 
-drawHouse(0, 0, 300, 100)
+    # Define some variables that will be useful for house construction
+    roof_height = house_height * 0.2
+    building_height = house_height * 0.8
+
+    # Draw building
+    drawRectangle(x, y, house_width, building_height)
+
+    # create variables needed for drawing the door
+    door_width = house_width / 8
+    door_height = house_height / 4
+
+    # Draw door
+    door_x = x+(house_width * 0.75)
+    door_y = y
+    drawRectangle(door_x, door_y, door_width, door_height)
+    drawCircle(door_x + door_width*3/4, door_y + door_height/2, 3)
+
+    # Draw roof
+    overhang = 20
+    drawTriangle(x-(overhang / 2), (y + building_height), (house_width + overhang), roof_height)
+
+    # Draw window
+    window_size = building_height / 2
+    window_x = (x+(house_width * 0.2))
+    window_y = y + (building_height / 3)
+    drawRectangle(window_x, window_y, window_size, window_size/2)
+
+    # Draw the window sections
+    goto(window_x + window_size/2, window_y)
+    pendown()
+    goto(window_x + window_size/2, window_y + window_size/2)
+    penup()
+    goto(window_x, window_y + window_size/4)
+    pendown()
+    goto(window_x + window_size, window_y + window_size/4)
+
+    penup()
+
+
+# drawHouse(-100, -40, 300, 300)  # uncomment to test the drawHouse function
 # drawTree(0, 0, 60)
+
+input()
